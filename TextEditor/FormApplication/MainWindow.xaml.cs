ed24ff2bd5d549e7d517dc181d2307d3e8afd87a
huytrinh <huytrinh93@yahoo.com>
2015-10-05 17:49:52 -0500

    Second project v1

---
+++ TextEditor/FormApplication/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Forms;
+using System.IO;
+
+namespace FormApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextDocument formBox = new TextDocument();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (!formBox.Save(saveFileDialog.FileName,textBox.Text))
+                {
+                    // http://msdn.microsoft.com/en-us/library/Aa984357
+                    System.Windows.Forms.MessageBox.Show("An error occurred saving the grades.", "Grader", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Multiselect = true;
+            //openFileDialog.InitialDirectory = "c:\\";
+            // http://msdn.microsoft.com/en-us/library/system.windows.forms.filedialog.filter
+            // http://msdn.microsoft.com/en-us/library/system.windows.forms.filedialog.filterindex.aspx
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox.Text = "";
+
+                formBox.Load(openFileDialog.FileName);
+                textBox.Text = formBox.ToString();
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
